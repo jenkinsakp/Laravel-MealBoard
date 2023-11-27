347d487ff3b3755eb9c0e44c103078382549ec35
jenkinsakp <unuavworhoakpevwe@gmail.com>
2023-11-27 15:31:21 +0400

    comments migration

--- database/migrations/2023_11_26_214023_comment.php
+++ database/migrations/2023_11_26_214023_comment.php
@@ -13,7 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('comment', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('menu_id')->references('id')->on('menu');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->text('comment');
+            $table->text('comment_type')->comment('check the type iof comment, could be anonymous, open to all e.t.c ');
+            $table->softDeletes();
+            $table->timestamps();
+
+
+        });
     }
 
     /**
@@ -23,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('menu');
     }
 };
